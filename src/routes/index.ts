e805409d2b5aeeafe375800b35e0dd39543502c8
Monica Apaza <mapaza@absencesoft.com>
2025-06-06 19:56:46 -0400

    adding post api

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -2,11 +2,13 @@ import { Router } from 'express';
 import healthRoutes from './health.routes';
 import codeOnDemandRoutes from './code-on-demand.routes';
 import userRoutes from './v1/user.routes';
+import postRoutes from './v1/post.routes';
 
 const router = Router();
 
 router.use('/health', healthRoutes);
 router.use('/code-on-demand', codeOnDemandRoutes);
 router.use('/v1/users', userRoutes);
+router.use('/v1/posts', postRoutes);
 
 export default router; 
